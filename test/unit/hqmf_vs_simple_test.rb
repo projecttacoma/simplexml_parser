0858b745aa2b31f08443789a6bbc7ac7ec6c2f12
Andre Quina <aquina@mitre.org>
2014-05-20 11:47:16 -0400

    fix issues with hqmf for comparison and update hash generation to replace child ids before building the hash

--- test/unit/hqmf_vs_simple_test.rb
+++ test/unit/hqmf_vs_simple_test.rb
@@ -19,7 +19,7 @@ class HQMFVsSimpleTest < Test::Unit::TestCase
   measure_files = File.join(HQMF_ROOT, '*.xml')
   
   Dir.glob(measure_files).each do | measure_filename |
-#    measure_name = /.*[\/\\]((ep|eh)_.*)\.xml/.match(measure_filename)[1]
+    # next unless (measure_filename.ends_with? 'MedianAdmitDecisionTime_eMeasure.xml')
     measure_name = File.basename(measure_filename, ".xml")
     define_method("test_#{measure_name}") do
       do_roundtrip_test(measure_filename, measure_name)
@@ -30,15 +30,14 @@ class HQMFVsSimpleTest < Test::Unit::TestCase
     puts ">> #{measure_name}"
     # parse the model from the V1 XML
     hqmf_model = HQMF::Parser.parse(File.open(measure_filename).read, HQMF::Parser::HQMF_VERSION_1)
+    # rebuild hqmf model so that source data criteria are different objects
+    hqmf_model = HQMF::Document.from_json(JSON.parse(hqmf_model.to_json.to_json))
 
+    # parse the model from the simple XML
     simple_xml = File.join(SIMPLE_XML_ROOT, "#{hqmf_model.cms_id}.xml")
     simple_xml_model = SimpleXml::Parser.parse(File.read(simple_xml), SimpleXml::Parser::SIMPLEXML_VERSION_1)
 
-    hqmf_model.all_data_criteria.sort! {|l,r| l.id <=> r.id}
-    simple_xml_model.all_data_criteria.sort! {|l,r| l.id <=> r.id}
-    hqmf_model.attributes.sort! {|l,r| l.name <=> r.name}
-    simple_xml_model.attributes.sort! {|l,r| l.name <=> r.name}
-
+    # ignore the attributes... these are not that important
     hqmf_model.instance_variable_set(:@attributes, [])
     simple_xml_model.instance_variable_set(:@attributes, [])
 
@@ -52,33 +51,34 @@ class HQMFVsSimpleTest < Test::Unit::TestCase
     referenced_ids = simple_xml_model.referenced_data_criteria.map(&:id)
     simple_xml_model.all_data_criteria.reject! {|dc| !referenced_ids.include? dc.id }
 
-    # Generate new IDs for data criteria in both models by hashing a subset of the fields.
-    # Then remap the original IDs created when each model was parsed to the common ID for
-    # comparison below.
+    # fix the descriptions on hqmf data criteria to align with better simpleXML descripions
+    # also remove empty units in hqmf
+    (hqmf_model.all_data_criteria + hqmf_model.source_data_criteria).each do |dc|
+      fix_hqmf_description(dc)
+      remove_empty_unit(dc)
+    end
+
+    # HQMF leaf preconditions sometimes have conjunction codes as well as a reference... 
+    # we want to clear those conjunction codes before comparison as they are not needed
+    hqmf_model.all_population_criteria.each do |pc|
+      clear_conjunctions_on_references(pc)
+    end
+
     remap_ids(hqmf_model)
     remap_ids(simple_xml_model)
 
-    # try to match IDs for data criteria that have changed
-    # simple_referenced_dc = simple_xml_model.referenced_data_criteria
-    # hqmf_referenced_dc = hqmf_model.referenced_data_criteria
+    simple_xml_model.all_population_criteria.each do |pc|
+      # replace HQMF ID for populations... that does not get set properly in the HQMF
+      pc.instance_variable_set(:@hqmf_id, hqmf_model.population_criteria(pc.id).hqmf_id)
 
-    # simple_referenced_dc.each {|dc| dc.instance_variable_set(:@description, '')}
-    # hqmf_referenced_dc.each {|dc| dc.instance_variable_set(:@description, '')}
+      # set title on DENEX to denominator since the title is bad out of HQMF
+      pc.instance_variable_set(:@title, 'Denominator') if pc.type == 'DENEX'
+    end
 
-    # orig_simple_keys = simple_referenced_dc.map {|dc| dc.id }
-    # dc_key_translation = get_dc_key_translation(hqmf_referenced_dc, simple_referenced_dc, {})
-    # orig_simple_keys.each {|k| puts "NO TRANSLATION FOR: #{k}" unless dc_key_translation[k]}
-
-    # simple_xml_model.all_population_criteria.each do |pc|
-    #   update_referenced_ids(pc, dc_key_translation)
-    # end
-
+    # COMPARE
 
     hqmf_json = JSON.parse(hqmf_model.to_json.to_json)
     simple_xml_json = JSON.parse(simple_xml_model.to_json.to_json)
-
-    # update_v1_json(hqmf_json)
-    # update_v2_json(simple_xml_json)
 
     diff = hqmf_json.diff_hash(simple_xml_json, true, true)
 
@@ -96,124 +96,134 @@ class HQMFVsSimpleTest < Test::Unit::TestCase
   end
 
   def remap_ids(measure_model)
-    idmap = Hash.new
 
-    # First pass calculates new IDs by hashing various attributes that should be identical in both models
-    measure_model.all_data_criteria.each do |dc|
-      # generate a SHA256 hash of key fields in the data criteria
-      sha256 = Digest::SHA256.new
-      sha256.update "#{measure_model.id}:#{measure_model.title}:"
-      sha256.update "#{dc.code_list_id}:"
-      sha256.update "#{dc.definition}:"
-      sha256.update "#{dc.negation}:"
-      sha256.update dc.status.nil? ? "<nil>:" : "#{dc.status}:"
-      sha256.update dc.negation_code_list_id.nil? ? "<nil>:" : "#{dc.negation_code_list_id}:"
-      sha256.update dc.value.nil? ? "<nil>:" : "#{dc.value.type}:"
-      if dc.subset_operators.nil?
-        sha256.update "<nil>:"
-      else
-        dc.subset_operators.each do |sso|
-          sha256.update "#{sso.type}:"
-        end
-      end
-      sha256.update dc.derivation_operator.nil? ? "<nil>:" : "#{dc.derivation_operator}:"
-      idmap[dc.id] = sha256.hexdigest
-    end
+    criteria_list = (measure_model.all_data_criteria + measure_model.source_data_criteria)
+    criteria_map = get_criteria_map(criteria_list)
 
-    # Second pass reassigns IDs using the hash-generated ID
-    measure_model.all_data_criteria.each do |dc|
-      if ! idmap[dc.id].nil?
-        dc.id = idmap[dc.id]
-      else
-        puts "IDMAP: No mapped ID for data criteria: #{dc.id}"
-      end
-      unless dc.temporal_references.nil?
-        dc.temporal_references.each do |tr|
-          if ! idmap[tr.reference.id].nil?
-            tr.reference.id = idmap[tr.reference.id]
-          else
-            puts "IDMAP: No mapped ID for temporal reference: #{tr.reference.id}"
-          end
-        end
-      end
-      unless dc.children_criteria.nil?
-        dc.children_criteria.each_index do |cci|
-          child_id = dc.children_criteria[cci]
-          if ! idmap[child_id].nil?
-            dc.children_criteria[cci] = idmap[child_id]
-          else
-            puts "IDMAP: No mapped ID for child criteria: #{child_id}"
-          end
-        end
+
+    # Normalize the HQMF model IDS
+    criteria_list.each do |dc|
+      dc.id = hash_criteria(dc, criteria_map)
+      dc.instance_variable_set(:@source_data_criteria, dc.id)
+      if dc.type == :derived
+        dc.instance_variable_set(:@title, dc.id)
       end
     end
 
-    # Now that the data criteria IDs have been remapped, we need to change the
-    # references to those data criteria in the population criteria.
     measure_model.all_population_criteria.each do |pc|
-      remap_preconditions(idmap, pc.preconditions)
+      remap_preconditions(criteria_map, pc.preconditions)
     end
-    puts "\nIDMAP DONE\n\n"
+
   end
 
-  def remap_preconditions(idmap, preconditions)
-    if preconditions.nil?
-      return
-    end
-
-    preconditions.each do |pc|
-      remap_preconditions(idmap, pc.preconditions)
-      unless pc.reference.nil?
-        if ! idmap[pc.reference.id].nil?
-          pc.reference.id = idmap[pc.reference.id]
-        else
-          puts "IDMAP: no mapped ID for #{pc.reference.id}"
+  def remap_preconditions(criteria_map, preconditions)
+    return if preconditions.nil?
+    preconditions.each do |precondition|
+      remap_preconditions(criteria_map, precondition.preconditions)
+      unless precondition.reference.nil?
+        if data_criteria = criteria_map[precondition.reference.id]
+          precondition.reference.id = data_criteria.id
         end
       end
     end
   end
 
-  # def get_dc_key_translation(hqmf_referenced_dc, simple_referenced_dc, dc_key_translation)
+  def hash_criteria(criteria, criteria_map)
+    return criteria.id unless criteria_map[criteria.id]
 
-  #   orig_count = dc_key_translation.keys.count
-  #   hqmf_referenced_dc.each do |dc|
-  #     hqmf_key = dc.id
-  #     match = simple_referenced_dc.select {|sdc| sdc.base_json.diff_hash(dc.base_json, true, true).empty?}
+    # generate a SHA256 hash of key fields in the data criteria
+    sha256 = Digest::SHA256.new
+    # uncomment to keep string undigested for comparison
+    #sha256 = ''
 
-  #     if (match.count > 0)
-  #       match = match.first
-  #       dc_key_translation[match.id] = hqmf_key
-  #     end
-  #   end
+    sha256 << "#{criteria.code_list_id}:"
+    sha256 << "#{criteria.definition}:"
+    sha256 << (criteria.status.nil? ? "<nil>:" : "#{criteria.status}:")
+    sha256 << "#{criteria.negation}:"
+    sha256 << (criteria.negation_code_list_id.nil? ? "<nil>:" : "#{criteria.negation_code_list_id}:")
 
-  #   simple_referenced_dc.each do |dc|
-  #     dc.id = dc_key_translation[dc.id] if dc_key_translation[dc.id]
-  #     if (dc.temporal_references)
-  #       dc.temporal_references.each do |ref|
-  #         ref.reference.id = dc_key_translation[ref.reference.id] if dc_key_translation[ref.reference.id]
-  #       end
-  #     end
-  #     if (dc.children_criteria)
-  #       dc.children_criteria.map! {|k| dc_key_translation[k] || k}
-  #     end
-  #   end
+    # build hashes of each complex child... these will update refereces to other data criteria as the hash is built
+    sha256 << (criteria.value.nil? ? "<nil>:" : "#{hash_values(criteria.value)}:")
+    sha256 << (criteria.children_criteria.nil? ? "<nil>:" : "#{hash_children(criteria.children_criteria, criteria_map)}:")
+    sha256 << (criteria.subset_operators.nil? ? "<nil>:" : "#{hash_subsets(criteria.subset_operators)}:")
+    sha256 << (criteria.temporal_references.nil? ? "<nil>:" : "#{hash_temporals(criteria.temporal_references, criteria_map)}:")
+    sha256 << (criteria.field_values.nil? ? "<nil>:" : "#{hash_fields(criteria.field_values)}:")
 
-  #   dc_key_translation = get_dc_key_translation(hqmf_referenced_dc, simple_referenced_dc, dc_key_translation) if (dc_key_translation.keys.count > orig_count)
+    sha256.hexdigest
+    #sha256
+  end
 
-  #   dc_key_translation
+  def hash_subsets(list)
+    Digest::SHA256.hexdigest list.map {|x| x.to_json.to_json}.join(',')
+  end
 
-  # end
+  def hash_temporals(list, criteria_map)
+    list.each do |t|
+      t.reference.id = hash_criteria(criteria_map[t.reference.id], criteria_map) if criteria_map[t.reference.id]
+    end
 
-  # def update_referenced_ids(precondition, dc_key_translation)
-  #   if (precondition.preconditions && !precondition.preconditions.empty?)
-  #     precondition.preconditions.each do |child|
-  #       update_referenced_ids(child, dc_key_translation)
-  #     end
-  #   else
-  #     if (precondition.respond_to?(:reference) && precondition.reference)
-  #       precondition.reference.id = dc_key_translation[precondition.reference.id] if dc_key_translation[precondition.reference.id]
-  #     end
-  #   end
-  # end
+    Digest::SHA256.hexdigest list.map {|x| x.to_json.to_json}.join(',')
+  end
+
+  def hash_children(list, criteria_map)
+    list.map! {|id| hash_criteria(criteria_map[id], criteria_map)} if list.select {|id| criteria_map[id]}.length == list.length
+    list.join(',')
+  end
+
+  def hash_fields(hash)
+    hash.values.each {|fv| fv.instance_variable_set(:@title,'')}
+    Digest::SHA256.hexdigest hash.to_json
+  end
+
+  def hash_values(value)
+    value.instance_variable_set(:@title, '') if value.type == 'CD'
+    Digest::SHA256.hexdigest value.to_json.to_json
+  end
+
+  # from hqmf the unit on counts can sometimes be " "... we want to clear these
+  def remove_empty_unit(dc)
+    if (dc.subset_operators)
+      dc.subset_operators.each do |subset|
+        subset.value.high.instance_variable_set(:@unit, nil) if subset.value.respond_to?(:high) && subset.value.try(:high).try(:unit).try(:strip).try(:empty?)
+        subset.value.low.instance_variable_set(:@unit, nil) if subset.value.respond_to?(:low) && subset.value.try(:low).try(:unit).try(:strip).try(:empty?)
+      end
+    end
+  end
+
+  def clear_conjunctions_on_references(precondition)
+    if (precondition.preconditions && !precondition.preconditions.empty?)
+      precondition.preconditions.each do |child|
+        clear_conjunctions_on_references(child)
+      end
+    else
+      if (precondition.respond_to?(:reference) && precondition.reference && precondition.conjunction_code)
+        precondition.instance_variable_set(:@conjunction_code,nil)
+      end
+    end
+  end
+
+  def fix_hqmf_description(dc)
+
+    orig = dc.description.dup
+    dc.description.sub!('Diagnostic Study Result','Diagnostic Study, Result') if dc.description.starts_with? "Diagnostic Study Result"
+    dc.description.sub!('Laboratory Test:','Laboratory Test, Result:') if dc.description.starts_with? "Laboratory Test:"
+    dc.description.sub!('Physical Exam:','Physical Exam, Finding:') if dc.description.starts_with? "Physical Exam:"
+    dc.description.sub!('Communication From Provider To Provider:','Communication: From Provider to Provider:') if dc.description.starts_with? "Communication From Provider To Provider:"
+    dc.description.sub!('Functional Status Result:','Functional Status, Result:') if dc.description.starts_with? "Functional Status Result:"
+    dc.description.sub!('Procedure Result:','Procedure, Result:') if dc.description.starts_with? "Procedure Result:"
+    dc.description.sub!('Medication Allergy:','Medication, Allergy:') if dc.description.starts_with? "Medication Allergy:"
+    dc.description.sub!('Medication Intolerance:','Medication, Intolerance:') if dc.description.starts_with? "Medication Intolerance:"
+    dc.description.sub!('Procedure Intolerance:','Procedure, Intolerance:') if dc.description.starts_with? "Procedure Intolerance:"
+
+    dc.instance_variable_set(:@specific_occurrence_const, SimpleXml::DataCriteria.format_so_const(dc.description)) if orig != dc.description
+  end
+
+  def get_criteria_map(criteria_list)
+    criteria_map = {}
+    criteria_list.each do |dc|
+      criteria_map[dc.id] = dc
+    end
+    criteria_map
+  end
 
 end
